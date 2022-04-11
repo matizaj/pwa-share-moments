411cf1daf40dbac59d6542cce979a46e7fec55a3
Mateusz <matzaj101@gmail.com>
2022-04-11 13:08:43 +0200

    add basic events to servce worker

--- public/sw.js
+++ public/sw.js
@@ -1,1 +1,11 @@
-﻿
+﻿self.addEventListener("install", (e) => {
+  console.log("[Service worker] install", e);
+});
+self.addEventListener("activate", (e) => {
+  console.log("[Service worker] activated", e);
+  return self.clients.claim();
+});
+
+self.addEventListener("fetch", (e) => {
+  console.log("[Service worker] fetching", e);
+});
