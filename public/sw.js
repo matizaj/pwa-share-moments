f5cba59e8dc1af7c423cc1ab4b7bf71e2b5c9a70
Mateusz <matzaj101@gmail.com>
2022-04-11 21:31:35 +0200

    add dynamic cache

--- public/sw.js
+++ public/sw.js
@@ -6,6 +6,7 @@
   "/src/js/app.js",
   "/src/js/feed.js",
   "/src/js/material.min.js",
+  "/src/images/main-image.jpg",
   "https://fonts.googleapis.com/css?family=Roboto:400,700",
   "https://fonts.googleapis.com/icon?family=Material+Icons",
   "https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css",
@@ -30,7 +31,13 @@ self.addEventListener("fetch", (e) => {
       if (response) {
         return response;
       } else {
-        return fetch(e.request);
+        return fetch(e.request).then((res) => {
+          console.log("res", res);
+          caches.open("dynamic").then((cache) => {
+            cache.put(e.request.url, res.clone());
+            return res;
+          });
+        });
       }
     })
   );
